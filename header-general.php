0569120d26135527d63581d402cac7213423da6a
Ashley Miller <ashley.miller@airship.co.uk>
2016-08-25 11:02:52 +0100

    background cover for page banners

--- header-general.php
+++ header-general.php
@@ -46,4 +46,5 @@
 	<?php
 	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 	?>
-	<div class="row twelve columns bannerImage" style="background: url(<?php echo $src[0]; ?> ); background-position:center center; height: 400px;"></div>
+	<div class="row twelve columns bannerImage" style="background: url(<?php echo $src[0]; ?> ); background-position:top center; height: 400px; -webkit-background-size: cover;
+  -moz-background-size: cover; -o-background-size: cover; background-size: cover;"></div>
